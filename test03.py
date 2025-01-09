1f44bde711ca82409b2ddff8a047efe7fb9f23e6
joseguifs <joseguilhermeagente007@gmail.com>
2025-01-09 18:21:08 -0300

    melhorando um pouco

--- test03.py
+++ test03.py
@@ -1,24 +1,28 @@
-class Carro:
-    rodas = 4
+class Veiculo:
 
     def __new__(cls, *args, **kargs):
         print("Alocando memória para criar e retorns uma nova instanca da classe")
-        instance = super(Carro, cls).__new__(cls)
+        instance = super(Veiculo, cls).__new__(cls)
         return instance
     
     def __init__(self,nome, cor, marca,ano):
         self.nome = nome
-        
         self.cor = cor 
         self.marca = marca 
-        self.ano = ano 
+        self.ano = ano
+        self.km = 0
+
 
     def apresentacao(self):
-        print(f"Esse é o carro {self.nome} da marca {self.marca} de cor {self.cor} de ano {self.ano}")
+        print(f"Esse é o carro {self.nome} da marca {self.marca} de cor {self.cor} do ano {self.ano} de {self.km}km")
+    
+    def acelerar(self):
+        velocidade = int(input(f"Deseja acelerar quanto no {self.nome}? "))
+        print(f"acelerando {velocidade}")
+        self.km = velocidade
+
     
 
 # guardando a referência do objeto em uma variavel simples
 
-carro1 = Carro("Corolla","Prata","Toyota",2025)
-carro1.apresentacao()
-    
+carro1 = Veiculo("Corolla","Prata","Toyota",2025)
